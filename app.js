8c37d8bc92e71179f2c97084b0b003b2ea237813
Daniel Owen <unlimiteddemi@gmail.com>
2025-01-24 00:49:37 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/database');
-const resumeRoutes = require('./routes/resumeRoutes');
-const assessmentRoutes = require('./routes/assessmentRoutes');
-const feedbackRoutes = require('./routes/feedbackRoutes');
-const profileRoutes = require('./routes/profileRoutes');
+// const resumeRoutes = require('./routes/resumeRoutes');
+// const assessmentRoutes = require('./routes/assessmentRoutes');
+// const feedbackRoutes = require('./routes/feedbackRoutes');
+// const profileRoutes = require('./routes/profileRoutes');
+const userRoutes = require('./routes/userRoutes');
+
 
 const app = express();
 
@@ -16,10 +18,7 @@ app.use(express.json()); // Add this to parse JSON request bodies
 connectDB();
 
 // Routes
-app.use('/api/resume', resumeRoutes);
-app.use('/api/assessment', assessmentRoutes);
-app.use('/api/feedback', feedbackRoutes);
-app.use('/api/profile', profileRoutes); // New Profile Routes
+app.use('/api/user', userRoutes);
 
 // Home route
 app.get('/', (req, res) => {
@@ -27,21 +26,6 @@ app.get('/', (req, res) => {
 });
 
 // Profile route
-app.get('/api/profile', async (req, res) => {
-    try {
-        const user = await User.findOne();
-        if (!user) {
-            return res.status(404).json({ message: 'No profile data yet' });
-        }
-        const profile = {
-            name: user.name,
-            email: user.email,
-            contactNumber: user.contactNumber
-        };
-        res.status(200).json({ profile });
-    } catch (error) {
-        res.status(500).json({ message: 'Error getting user profile' });
-    }
-});
+// 
 
 module.exports = app;
--- routes/resumeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadResume, upload, setCareerGoal } = require('../controllers/resumeController');
-
-// Route for uploading resumes
-router.post('/upload', upload.single('file'), uploadResume);
-router.post('/goal', setCareerGoal);
-
-module.exports = router;
